280ffe41a3623a0e0f150c9dfb309da18028a833
Turbo_Jr <a.kovalev87@gmail.com>
2017-06-23 15:29:06 +0300

    добавил блок приглашения поработать

--- Python_Quick_Start.py
+++ Python_Quick_Start.py
@@ -1,3 +1,18 @@
-#Greetings
+#Приветствие
+print('Great Python Program')
 print('Привет программист!')
-input()
+name = input("Как Вас зовут: ")
+
+print(name, ', добро пожаловать в мир Python!')
+
+#Приглашение поработать
+answer = input("Давайте поработаем? (Y/N/q) ")
+
+if answer == 'Y':
+    print('отлично!')
+    print('я умею:')
+elif answer == 'N':
+    print('До свидания!')
+else:
+    print('Неизвестный ответ')
+
